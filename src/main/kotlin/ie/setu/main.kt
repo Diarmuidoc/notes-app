8e903f74ff0d7b2c275539ab4013582fa199e823
Diarmuid <20071587@mail.wit.ie>
2024-10-04 14:52:35 +0100

    Implement UserInput utility for robust console IO

--- src/main/kotlin/ie/setu/main.kt
+++ src/main/kotlin/ie/setu/main.kt
@@ -1,5 +1,7 @@
 package ie.setu
 
+import utils.readIntNotNull
+import utils.readNextInt
 import java.lang.System.exit
 
 fun main() {
@@ -19,8 +21,8 @@ fun mainMenu() : Int {
           > ----------------------------------
           > |   0) Exit                      |
           > ----------------------------------
-          > ==>> """.trimMargin(">"))
-    return readln().toInt()
+          >""".trimMargin(">"))
+    return readNextInt("==>>")
 }
 
 fun runMenu() {
