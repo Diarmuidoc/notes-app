a6930555922c06c52a8bdc8329add41586a24d91
Diarmuid <20071587@mail.wit.ie>
2024-10-04 15:29:03 +0100

    Fix readNext bug

--- src/main/kotlin/utils/UserInput.kt
+++ src/main/kotlin/utils/UserInput.kt
@@ -23,8 +23,30 @@ fun readNextChar(prompt: String?): Char {
         try{
             print(prompt)
             return readln().first()
-        } catch (e: NumberFormatException) {
+        } catch (e: Exception) {
             System.err.println("\tEnter a character please.")
+        }
+    } while (true)
+}
+
+fun readNextDouble(prompt: String?): Double{
+    do{
+        try{
+            print(prompt)
+            return readln().toDouble()
+        } catch (e: NumberFormatException){
+            System.err.println("\tEnter a number please")
+        }
+    } while (true)
+}
+
+fun readNextFloat(prompt: String?): Float{
+    do{
+        try{
+            print(prompt)
+            return readln().toFloat()
+        } catch (e: NumberFormatException){
+            System.err.println("\tEnter a number please")
         }
     } while (true)
 }
