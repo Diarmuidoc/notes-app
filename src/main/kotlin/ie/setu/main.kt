e8db35331dc71c9d0c41a4055ede9b6e89ce9ca0
Diarmuid <20071587@mail.wit.ie>
2024-10-04 14:34:16 +0100

    Add Template Menu

---
+++ src/main/kotlin/ie/setu/main.kt
@@ -0,0 +1,60 @@
+package ie.setu
+
+import java.lang.System.exit
+
+fun main() {
+    runMenu()
+}
+
+fun mainMenu() : Int {
+    println("""
+          > ----------------------------------
+          > |        NOTE KEEPER APP         |
+          > ----------------------------------
+          > | NOTE MENU                      |
+          > |   1) Add a note                |
+          > |   2) List all notes            |
+          > |   3) Update a note             |
+          > |   4) Delete a note             |
+          > ----------------------------------
+          > |   0) Exit                      |
+          > ----------------------------------
+          > ==>> """.trimMargin(">"))
+    return readln().toInt()
+}
+
+fun runMenu() {
+    do{
+        val option = mainMenu()
+        when (option){
+            1 -> addNote()
+            2 -> listNotes()
+            3 -> updateNote()
+            4 -> deleteNote()
+            0 -> exitApp()
+            else -> println("Invalid option entered: ${option}")
+        }
+    } while (true)
+}
+
+fun addNote(){
+    println("You chose Add Note")
+}
+
+fun listNotes(){
+    println("You chose List Notes")
+}
+
+fun updateNote(){
+    println("You chose Update Note")
+}
+
+fun deleteNote(){
+    println("You chose Delete Note")
+}
+
+fun exitApp(){
+    println("Exiting...Bye")
+    exit(0)
+}
+
