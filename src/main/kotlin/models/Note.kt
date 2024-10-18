7a501498377236207c39b922eeaf13ccd3d37207
Diarmuid <20071587@mail.wit.ie>
2024-10-18 13:01:15 +0100

    Add new Model class, Note

---
+++ src/main/kotlin/models/Note.kt
@@ -0,0 +1,11 @@
+package models
+
+class Note {
+    data class Note(
+        val noteTitle: String,
+        val notePriority: Int,
+        val noteCategory: String,
+        val isNoteArchived: Boolean){
+
+    }
+}
